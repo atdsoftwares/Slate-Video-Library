8baa1acfd3853ebe47c60b80423307453a9636e1
iprankurpandey <pprankur@gmail.com>
2022-05-25 01:23:46 +0530

    fix : homepage hero banner

---
+++ slate-video-library/src/Components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Hero.css";
+function Hero() {
+  return (
+    <div>
+      <div className="hero">
+        <span className="hero-text">
+          <span className="hero-heading">Watch </span>
+          <span className="hero-para"> Videos and take notes </span>
+          <span className="hero-para2">
+            Gamify your learning.
+            <Link to="explore">
+              <button className="btn btn-success">Explore </button>
+            </Link>
+          </span>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
