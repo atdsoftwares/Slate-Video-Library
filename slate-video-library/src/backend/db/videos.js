b4f53deefc64fd449c794d51c5cbfd49b06beeb9
iprankurpandey <pprankur@gmail.com>
2022-05-25 01:23:27 +0530

    video db

--- slate-video-library/src/backend/db/videos.js
+++ slate-video-library/src/backend/db/videos.js
@@ -1,3 +1,5 @@
+import { v4 as uuid } from "uuid";
+
 /**
  * Videos Database can be added here.
  * You can add videos of your wish with different attributes
@@ -5,17 +7,122 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: uuid(),
+    title: "Technology :amazing ",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      " New Technology that will blow your mind : In this video youtuber explains about some technologies that will really blow your mind and you can't believe it.",
+    catagory: "Technology",
+    thumbnailUrl:
+      "https://i.ytimg.com/an_webp/7vQznLKEfBs/mqdefault_6s.webp?du=3000&sqp=CO7r05IG&rs=AOn4CLCUYOL2XfPA92EDnvR5zlBpBfxwPw",
+    videoUrl: "https://www.youtube.com/embed/rQFZdcPphwU",
+    creator_pic:
+      "https://yt3.ggpht.com/ytc/AKedOLSR1kOOGcsmya2i1HCjJUYDwtN6XCGYMEJYD4HL=s68-c-k-c0x00ffffff-no-rj",
+  },
+
+  {
+    _id: uuid(),
+    title: "The World in 2050 ",
+    description:
+      "In this video youtuber explains about some technologies that will really blow your mind and you can't believe it.",
+    catagory: "Technology",
+    thumbnailUrl:
+      "https://i.ytimg.com/an_webp/Oa9aWdcCC4o/mqdefault_6s.webp?du=3000&sqp=CJTS05IG&rs=AOn4CLCg19nv7-86uWJQnubxJtTjwzT-RQ",
+    videoUrl: "https://www.youtube.com/embed/EJi3Vxdyqv0",
+    creator_pic:
+      "https://yt3.ggpht.com/7BZtp1qb_yK4JhwqAc6urnssMYJR98f8274pFh5UKa3_wrzG3Qsmy_QieQO_iV0ac4X8_CIJ3Yw=s68-c-k-c0x00ffffff-no-rj",
+  },
+
+  {
+    _id: uuid(),
+    title: "15 Old Technologies",
+    description:
+      " In this video youtuber explains about some technologies that will really blow your mind and you can't believe it.",
+    catagory: "Technology",
+    thumbnailUrl:
+      "https://i.ytimg.com/an_webp/JoTtD3ZSueU/mqdefault_6s.webp?du=3000&sqp=CIDc05IG&rs=AOn4CLCk2y9y4sDLQZ6CIMVavpJRPsoG3A",
+    videoUrl: "https://www.youtube.com/embed/ldwr0HXpDSo",
+    creator_pic:
+      "https://yt3.ggpht.com/ytc/AKedOLQh0YhlrGYPYpZemAGl__vrKCsRQxCOXplFDgcC_Q=s68-c-k-c0x00ffffff-no-rj",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: uuid(),
+    title: "Amazing Car Technology ",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      " In this video youtuber explains about some technologies that will really blow your mind and you can't believe it.",
+    catagory: "Technology",
+    thumbnailUrl:
+      "https://i.ytimg.com/an_webp/iGkl34KTRaU/mqdefault_6s.webp?du=3000&sqp=CPjr05IG&rs=AOn4CLAAdNH9tAD8MK9MmF7UFy6BYoxUFg",
+    videoUrl: "https://www.youtube.com/embed/_jdPC5F83Fo",
+    creator_pic:
+      "https://yt3.ggpht.com/ytc/AKedOLQ5DCS55gDHcjd5NcL0Rjvu5ghRopQo-zKP5giPDg=s68-c-k-c0x00ffffff-no-rj",
+  },
+
+  {
+    _id: uuid(),
+    title: "Maths for coding ",
+    description: " In this video youtuber explains about coding.",
+    catagory: "Coding",
+    thumbnailUrl:
+      "https://i.ytimg.com/an_webp/-KbuVuVDzCc/mqdefault_6s.webp?du=3000&sqp=CITu05IG&rs=AOn4CLAN7iYdv-tCoL5aYFNqJo6xSgCVQw",
+    videoUrl: "https://www.youtube.com/embed/-KbuVuVDzCc",
+    creator_pic:
+      "https://yt3.ggpht.com/j_ZCn6PJZufZpCMRI9zRxWxnGkPY_PY5erbypulqeRPyPA2jZhGaTVVgyJH5NI2KPkKQRqeEneg=s68-c-k-c0x00ffffff-no-rj",
+  },
+
+  {
+    _id: uuid(),
+    title: "How to apporach coding? ",
+    description: " In this video youtuber explains about coding.",
+    catagory: "Coding",
+    thumbnailUrl:
+      "https://i.ytimg.com/an_webp/0OQtLP3Q-PE/mqdefault_6s.webp?du=3000&sqp=CIDm05IG&rs=AOn4CLA4YTgZF1XcNBEfGK-tQzsUCjYykA",
+    videoUrl: "https://www.youtube.com/embed/0OQtLP3Q-PE",
+    creator_pic:
+      "https://yt3.ggpht.com/j_ZCn6PJZufZpCMRI9zRxWxnGkPY_PY5erbypulqeRPyPA2jZhGaTVVgyJH5NI2KPkKQRqeEneg=s68-c-k-c0x00ffffff-no-rj",
+  },
+
+  {
+    _id: uuid(),
+    title: "Tesla ,SpaceX & Elon Musk ",
+    description: " In this video youtuber explains about coding.",
+    catagory: "Tedx",
+    thumbnailUrl:
+      "https://i.ytimg.com/an_webp/IgKWPdJWuBQ/mqdefault_6s.webp?du=3000&sqp=CMzx05IG&rs=AOn4CLCV5H2K2EOv2SEeMSQIdp2il-qP4w",
+    videoUrl: "https://www.youtube.com/embed/IgKWPdJWuBQ",
+    creator_pic:
+      "https://yt3.ggpht.com/ytc/AKedOLSAltH3yarKh0eytWLSLtB9-kPpMLM5EejH-GAZIg=s68-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    _id: uuid(),
+    title: "Master your mindset ",
+    description: " In this video youtuber explains about coding.",
+    catagory: "Tedx",
+    thumbnailUrl:
+      "https://i.ytimg.com/an_webp/4AzpmZ7AjaQ/mqdefault_6s.webp?du=3000&sqp=COzi05IG&rs=AOn4CLDJHTayBlKjywvUE-ai8g3Kk6_A0Q",
+    videoUrl: "https://www.youtube.com/embed/4AzpmZ7AjaQ",
+    creator_pic:
+      "https://yt3.ggpht.com/ytc/AKedOLSAltH3yarKh0eytWLSLtB9-kPpMLM5EejH-GAZIg=s68-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    _id: uuid(),
+    title: "Cloud Computing",
+    description: " In this video youtuber explains about coding.",
+    catagory: "Technology",
+    thumbnailUrl:
+      "https://i.ytimg.com/an_webp/M988_fsOSWo/mqdefault_6s.webp?du=3000&sqp=CP7Y05IG&rs=AOn4CLA7-pM1BQfacMmDVu1IuG1lcxP8oA",
+    videoUrl: "https://www.youtube.com/embed/M988_fsOSWo",
+    creator_pic:
+      "https://yt3.ggpht.com/ytc/AKedOLSAltH3yarKh0eytWLSLtB9-kPpMLM5EejH-GAZIg=s68-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    _id: uuid(),
+    title: " technology and life  ",
+    description: " In this video youtuber explains about coding.",
+    catagory: "Technology",
+    thumbnailUrl:
+      "https://i.ytimg.com/an_webp/EJi3Vxdyqv0/mqdefault_6s.webp?du=3000&sqp=CPvR05IG&rs=AOn4CLC4wwVTrKsXgSTMRsb_D2c_JCPHEw",
+    videoUrl: "https://www.youtube.com/embed/EJi3Vxdyqv0",
+    creator_pic:
+      "https://yt3.ggpht.com/j_ZCn6PJZufZpCMRI9zRxWxnGkPY_PY5erbypulqeRPyPA2jZhGaTVVgyJH5NI2KPkKQRqeEneg=s68-c-k-c0x00ffffff-no-rj",
   },
 ];
