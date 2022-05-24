9aa0029417942c3f0ebae6109d0c04ea58dc33c2
iprankurpandey <pprankur@gmail.com>
2022-05-24 12:37:16 +0530

    added page strcuture fpr clean code

---
+++ slate-video-library/src/Pagestructure.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Homepage from "./Pages/Homepage/Homepage";
+
+function Pagestructure() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default Pagestructure;
