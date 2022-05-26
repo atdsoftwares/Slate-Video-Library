bed9e62d3c7d95b82b53d2a58e0db3083861b8c3
iprankurpandey <pprankur@gmail.com>
2022-05-26 19:33:22 +0530

    SIngle video page BUILD

---
+++ slate-video-library/build/precache-manifest.d0cd7c24c4a4efa3f5b1e9b41b2cd9e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8ad11098960d63e0de3",
+    "url": "/static/css/main.084ca2b4.chunk.css"
+  },
+  {
+    "revision": "e8ad11098960d63e0de3",
+    "url": "/static/js/main.e8ad1109.chunk.js"
+  },
+  {
+    "revision": "1f6f3f1b3cfb5d6ed264",
+    "url": "/static/js/1.1f6f3f1b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5176a9887892da1157a781140e171ee8",
+    "url": "/index.html"
+  }
+];
--- slate-video-library/build/service-worker.js
+++ slate-video-library/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74dd2aa37c963e5ed8cc1767e81f1fe1.js"
+  "/precache-manifest.d0cd7c24c4a4efa3f5b1e9b41b2cd9e7.js"
 );
 
 workbox.clientsClaim();
