a01e4b04b810d2c86187857d6611dbcb17ec901d
iprankurpandey <pprankur@gmail.com>
2022-05-27 21:20:20 +0530

    removed some burden from this fiel and separated fucntions

--- slate-video-library/src/Components/Context/SingleVideopageContext.js
+++ slate-video-library/src/Components/Context/SingleVideopageContext.js
@@ -20,39 +20,23 @@ function SingleVideopageContext({ children }) {
     switch (action.type) {
       case "SINGLEVIDEODATA":
         return { ...state, videoData: action.payload };
-      case "SINGLEVIDEOLIKES":
-        return { ...state, SinglevideoLikes: action.payload };
-      case "WATCHLATER":
-        return { ...state, SinglevideoWatchLater: action.payload };
       case "MODALSTATE":
         return { ...state, modalState: action.payload };
       case "INPUTSTATE":
         return { ...state, inputState: action.payload };
-      case "PLAYLIST":
-        return { ...state, playlists: action.payload };
       default:
         return state;
     }
   }
   const [state, dispatch] = useReducer(reducerFn, {
     videoData: [],
-    SinglevideoLikes: [],
-    SinglevideoWatchLater: [],
     modalState: "none",
     inputState: "",
-    playlists: [],
   });
 
-  const {
-    videoData,
-    SinglevideoLikes,
-    SinglevideoWatchLater,
-    modalState,
-    inputState,
-    playlists,
-  } = state;
+  const { videoData, modalState, inputState } = state;
 
-  // get single video data from url parameters
+  // get single video complete data from url parameters
   async function getSingleVideos() {
     try {
       await axios.get(`/api/video/${videoId}`).then((response) =>
@@ -70,68 +54,15 @@ function SingleVideopageContext({ children }) {
     getSingleVideos();
   }, [videoId]);
 
-  // set likes for individual single video
-
-  const likedVideo = async (videoData, dispatch) => {
-    const response = await axios({
-      method: "POST",
-      url: `/api/user/likes`,
-      headers: { authorization: localStorage.getItem("token") },
-      data: { video: videoData },
-    });
-
-    dispatch({ type: "SINGLEVIDEOLIKES", payload: response.data.likes });
-  };
-
-  // add to watch later  a individual single video
-
-  const watchLaterVideo = async (videoData, dispatch) => {
-    const response = await axios({
-      method: "POST",
-      url: `/api/user/watchlater`,
-      headers: { authorization: localStorage.getItem("token") },
-      data: { video: videoData },
-    });
-
-    dispatch({
-      type: "WATCHLATER",
-      payload: response.data.watchlater,
-    });
-  };
-
-  // make a playlist
-  async function postPlaylist() {
-    try {
-      const response = await axios({
-        method: "POST",
-        url: `/api/user/playlists`,
-        headers: { authorization: localStorage.getItem("token") },
-        data: {
-          playlist: { playlistName: inputState },
-        },
-      });
-      dispatch({
-        type: "PLAYLIST",
-        payload: response.data.playlists,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   return (
     <div>
       <singleVideoContext.Provider
         value={{
           setVideoId,
           videoData,
-          dispatch,
-          likedVideo,
-          watchLaterVideo,
           modalState,
           inputState,
-          playlists,
-          postPlaylist,
+          dispatch,
         }}
       >
         {children}
