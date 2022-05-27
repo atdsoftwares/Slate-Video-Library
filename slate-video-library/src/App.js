2ecf9d5104d1ed3f1a5952016c2a3ab9d0a8fd5e
iprankurpandey <pprankur@gmail.com>
2022-05-27 21:17:05 +0530

    fix :  updated files and structure

--- slate-video-library/src/App.js
+++ slate-video-library/src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Explorepage from "./Pages/Explorepage/Explorepage";
+import Historypage from "./Pages/Historypage/Historypage";
 import Homepage from "./Pages/Homepage/Homepage";
+import Likespage from "./Pages/Likespage/Likespage";
+import Playlistspage from "./Pages/Playlistspage/Playlistspage";
 import SingleVideopage from "./Pages/SingleVideoPage/SingleVideopage";
+import Watchlaterpage from "./Pages/Watchlaterpage/Watchlaterpage";
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
         <Route exact path="/" element={<Homepage />} />
         <Route exact path="/explore" element={<Explorepage />} />
         <Route exact path="/explore/:_id" element={<SingleVideopage />} />
+        <Route exact path="/playlists" element={<Playlistspage />} />
+        <Route exact path="/likes" element={<Likespage />} />
+        <Route exact path="/watchlater" element={<Watchlaterpage />} />
+        <Route exact path="/history" element={<Historypage />} />
       </Routes>
     </div>
   );
