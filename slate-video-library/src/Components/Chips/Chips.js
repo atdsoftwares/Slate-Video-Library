a679c04046f8a74a692302886bfdf0c93e18439a
iprankurpandey <pprankur@gmail.com>
2022-05-30 19:38:32 +0530

    feat :  sort explore page data with chips and searchbar

--- slate-video-library/src/Components/Chips/Chips.js
+++ slate-video-library/src/Components/Chips/Chips.js
@@ -1,12 +1,46 @@
 import React from "react";
+import { useExplorePageContext } from "../Context/ExplorepageContext";
 import "./Chips.css";
 function Chips() {
+  const { dispatch, state, isActive } = useExplorePageContext();
+  const { rating, search, stock } = state;
+
   return (
     <div className="chips-container">
-      <div className="chips">JS</div>
-      <div className="chips">JS</div>
-      <div className="chips">JS</div>
-      <div className="chips">JS</div>
+      <input
+        type="submit"
+        // className="chips"
+        className={isActive ? "chips2" : "chips"}
+        placeholder="Technology"
+        value="Technology"
+        onClick={() =>
+          dispatch({
+            type: "Technology",
+          })
+        }
+      />
+      <input
+        type="submit"
+        className="chips"
+        placeholder="Technology"
+        value="Tedx"
+        onClick={() =>
+          dispatch({
+            type: "Tedx",
+          })
+        }
+      />
+      <input
+        type="submit"
+        className="chips"
+        placeholder="Technology"
+        value="Coding"
+        onClick={() =>
+          dispatch({
+            type: "Coding",
+          })
+        }
+      />
     </div>
   );
 }
